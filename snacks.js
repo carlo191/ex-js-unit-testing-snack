01d39e361cec203b0a14b722c828810434c84e54
Carlo Elvisi <carloelvisi01@outlook.it>
2025-04-10 16:08:13 +0200

    fix snack 1

--- snacks.js
+++ snacks.js
@@ -1,5 +1,5 @@
 function getInitials(nomeCompleto) {
-  const [nome, cognome] = nomeCompleto.split(" ");
+  const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== "");
   return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
 }
 
--- snacks.test.js
+++ snacks.test.js
@@ -2,4 +2,6 @@ const { getInitials } = require("./snacks");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
   expect(getInitials("Mario Rossi")).toBe("M.R.");
+  expect(getInitials("luigi bianchi")).toBe("L.B.");
+  expect(getInitials("Maria  Verdi")).toBe("M.V.");
 });
