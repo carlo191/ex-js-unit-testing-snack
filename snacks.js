8e90230beeaf9ba9d2fbe7a3781abcebdb8d4ab9
Carlo Elvisi <carloelvisi01@outlook.it>
2025-04-10 16:11:56 +0200

    snack 2

--- snacks.js
+++ snacks.js
@@ -2,7 +2,11 @@ function getInitials(nomeCompleto) {
   const [nome, cognome] = nomeCompleto.split(" ").filter((str) => str !== "");
   return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
 }
+function createSlug(stringa ){
+    return stringa.toLowerCase();
+}
 
 module.exports = {
   getInitials,
+  createSlug
 };
--- snacks.test.js
+++ snacks.test.js
@@ -1,7 +1,11 @@
-const { getInitials } = require("./snacks");
+const { getInitials, createSlug } = require("./snacks");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
   expect(getInitials("Mario Rossi")).toBe("M.R.");
   expect(getInitials("luigi bianchi")).toBe("L.B.");
   expect(getInitials("Maria  Verdi")).toBe("M.V.");
+});
+
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+  expect(createSlug("Nome")).toBe("nome");
 });
